a0f67588858c0ad08cfb7b10ec04efb57275d8ea
lvanngo <linh.vanngo@chargepoint.com>
2017-03-24 15:36:20 -0700

    remove showLabels

--- src/pie-chart/simple-pie-chart.component.ts
+++ src/pie-chart/simple-pie-chart.component.ts
@@ -21,7 +21,6 @@ import { BaseChartComponent } from '../common/base-chart.component';
         class="pie chart">
         <svg:g ngx-charts-pie-series
           [colors]="colors"
-          [showLabels]="labels"
           [series]="results"
           [innerRadius]="innerRadius"
           [activeEntries]="activeEntries"
